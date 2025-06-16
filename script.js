160a71892ccf7d6da1b0e3b43f97be29add807c0
matheusozom <152895983+matheusozom@users.noreply.github.com>
2025-06-16 17:00:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-document.getElementById('capa').onclick = function() {
+document.getElementById('capa').onclick = async function() {
   const audio = document.getElementById('bg-music');
   audio.play();
-  
+
+  try {
+    await document.documentElement.requestFullscreen();
+    await screen.orientation.lock("landscape-primary");
+  } catch (err) {
+    console.log("O bloqueio de orientação não é suportado neste navegador.");
+  }
+
   document.querySelector('.swiper').classList.add("active");
   this.classList.add("hidden");
-};
-
-document.getElementById('go-gifts').onclick = function() {
-  document.querySelector('.swiper').classList.add("active");
 };
